4c7aa141275dcb2fc0e1217d894e03919b4e19cc
Showrove Roy <showrove200@gmail.com>
2022-11-06 16:52:51 +0600

    user read & delete done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -20,10 +20,23 @@ async function run() {
   try {
     const usersList = client.db("simpleNodeCRUD").collection("New_Users_List");
 
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersList.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersList.insertOne(user);
-      console.log(result);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersList.deleteOne(query);
       res.send(result);
     });
   } finally {
