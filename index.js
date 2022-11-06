061ef37af4088a412c91d82404f1bcbf92e119db
Showrove Roy <showrove200@gmail.com>
2022-11-06 14:29:43 +0600

    get data from client and add DB

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+
+// mongo db connect code
+const uri =
+  "mongodb+srv://dbUser1:TDj8KmPX2GoIqJCA@cluster0.in3ib7y.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usersList = client.db("simpleNodeCRUD").collection("New_Users_List");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersList.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("This simple crud server is running");
